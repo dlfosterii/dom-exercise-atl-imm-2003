85474310c019ecbff6bbbf15d841cfac9c2e4998
David Foster <dlfosterii@yahoo.com>
2020-04-22 12:57:22 -0400

    exercise complete with forEach

--- scripts/index.js
+++ scripts/index.js
@@ -6,3 +6,21 @@ const addresses = [
     "https://duckduckgo.com"
 ];
 
+
+const linkContainer = document.querySelector('.js-link-container')
+
+const ul = document.createElement('ul')
+
+function createListItem(address) {
+    const li = document.createElement('li');
+    const item = document.createElement('a')
+    item.setAttribute('href', address)
+    item.textContent = address
+    li.appendChild(item)
+    ul.appendChild(li)
+
+}
+
+addresses.forEach(createListItem)
+linkContainer.appendChild(ul)
+
